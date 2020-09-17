6e944a6107a312315320a59ce9975d9f3387108f
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-17 20:42:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
-first_number = 0
-second_number = 1
+first_number = 8
+second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
-quotient
+quotient = first_number / second_number
